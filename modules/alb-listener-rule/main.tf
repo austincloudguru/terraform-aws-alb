14be6e3a62e4b79108a0a3e4c10c6ca86c651fc9
Mark Honomichl <mark@austincloud.guru>
2020-02-14 13:19:01 -0600

    Initial add of the listener Rules

---
+++ modules/alb-listener-rule/main.tf
@@ -0,0 +1,64 @@
+resource "aws_lb_target_group" "this" {
+  name     = var.name
+  port     = var.port
+  protocol = var.protocol
+  vpc_id   = var.vpc_id
+
+  dynamic "stickiness" {
+    for_each = var.stickiness
+    content {
+      type = lookup(stickiness.value, "type", null)
+      cookie_duration = lookup(stickiness.value, "cookie_duration", null)
+    }
+  }
+
+  dynamic "health_check" {
+    for_each = var.health_check
+    content {
+      interval          = lookup(health_check.value, "interval", null)
+      path              = lookup(health_check.value, "path", null)
+      timeout           = lookup(health_check.value, "timeout", null)
+      healthy_threshold = lookup(health_check.value, "healthy_threshold", null)
+      port              = lookup(health_check.value, "port", null)
+      protocol          = lookup(health_check.value, "protocol", null)
+      matcher           = lookup(health_check.value, "matcher", null)
+    }
+  }
+
+  tags = merge(
+    {
+      "Name" = var.name
+    },
+    var.tags
+  )
+}
+
+resource "aws_lb_listener_rule" "this" {
+  listener_arn = var.listener_arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+
+  dynamic "condition" {
+    for_each = var.host_header
+    content {
+      host_header = lookup(condition.value, "host_header", null )
+    }
+  }
+
+   dynamic "condition" {
+    for_each = var.http_header
+    content {
+      http_header_name = lookup(condition.value, "http_header_name", null)
+      values = lookup(condition.value, "values", null)
+    }
+  }
+}
+
+resource "aws_lb_listener_certificate" "this" {
+  count = var.certificate_arn ? 1 : 0
+  certificate_arn = var.certificate_arn
+  listener_arn    = var.listener_arn
+}
---
+++ modules/alb-listener-rule/variables.tf
@@ -0,0 +1,102 @@
+variable "name" {
+  description = "The name of the target group."
+  type = string
+}
+
+variable "port" {
+  description = "The port on which targets receive traffic, unless overridden when registering a specific target."
+  type = string
+  default = 80
+}
+
+variable "protocol" {
+  description = "The protocol to use for routing traffic to the targets."
+  type = string
+  default = "HTTP"
+}
+
+variable "vpc_id" {
+  description = "The identifier of the VPC in which to create the target group."
+  type = string
+}
+
+variable "stickiness" {
+  description = " A Stickiness block."
+  type = list(object({
+    type = string
+    cookie_duration = number
+  }))
+  default = [{
+    type = lb_cookie
+    cookie_duration = 86400
+  }]
+}
+
+variable "health_check" {
+  description = "Listener Rule Health Check"
+  type = list(object({
+    interval          = number
+    path              = string
+    timeout           = number
+    healthy_threshold = number
+    port              = number
+    protocol          = string
+    matcher           = string
+  }))
+  default = [{
+    interval          = 60
+    path              = "/"
+    timeout           = 5
+    healthy_threshold = 2
+    port              = 80
+    protocol          = "HTTP"
+    matcher           = "200"
+  }]
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "listener_arn" {
+  description = "The ARN of the listener to which to attach the rule."
+  type        = string
+}
+
+variable "host_header" {
+  description = "Contains a single value item which is a list of host header patterns to match."
+  type = list(object({
+     host_header = list(string)
+  }))
+  default = []
+}
+
+variable "http_header" {
+  description = "HTTP headers to match."
+  type = list(object({
+    http_header_name = string
+    values = list(string)
+  }))
+  default = []
+}
+
+//variable "condition" {
+//  description = "An Access Logs block"
+//  type = list(object({
+//    host_header = list(string)
+//    http_header = list(object)
+//    http_request_method = list(string)
+//    path_pattern = list(string)
+//    query_string = list(object)
+//    source_ip = list(string)
+//  }))
+//  default = []
+//}
+
+variable "certificate_arn" {
+  description = "The ARN of the certificate to attach to the listener."
+  type = string
+  default = ""
+}
