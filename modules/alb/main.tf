c7196e4f94cdb279b9fa22b6171fec0673c35f29
Mark Honomichl <mark@austincloud.guru>
2020-02-14 10:30:47 -0600

    Added the egress rule

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -10,6 +10,15 @@ resource "aws_security_group" "this" {
   )
 }
 
+resource "aws_security_group_rule" "egress" {
+  security_group_id = aws_security_group.this.id
+  type = "egress"
+  from_port = 0
+  protocol = "-1"
+  to_port = 0
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_lb" "this" {
   name                       = var.name
   load_balancer_type         = "application"
