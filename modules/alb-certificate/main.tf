4f13ff8398c6943408635a8f9ef667c40b77ef9b
Mark Honomichl <mark@honomichl.net>
2020-08-25 11:03:23 -0500

    Updated the code to be compatible with AWS provider 3.0

--- modules/alb-certificate/main.tf
+++ modules/alb-certificate/main.tf
@@ -4,14 +4,22 @@ resource "aws_acm_certificate" "this" {
 }
 
 resource "aws_route53_record" "this" {
-  name    = aws_acm_certificate.this.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.this.domain_validation_options.0.resource_record_type
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
   zone_id = var.zone_id
-  records = [aws_acm_certificate.this.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
-  validation_record_fqdns = [aws_route53_record.this.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.this : record.fqdn]
 }
